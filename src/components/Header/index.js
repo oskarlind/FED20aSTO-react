59cfe87d42c5ec97d4c36cf58d0883dfaba6ee87
Oskar Lind <oskarlind@MBPsomthorOskar.lan>
2020-10-19 14:12:36 +0200

    Deployed to prod

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,6 +5,7 @@ import styles from './index.module.css'
 const Header = () => {
     return <header>
         <h2 className={styles.h2}>Welcome to the recipe website</h2>
+        <p>The best recipes - all in one place!</p>
         <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/order">Order form</Link></li>
